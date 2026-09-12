a39392fdad3e36d649e68175df532093472db488
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-09-12 16:06:31 +0200

    Get users preference endpoint set

--- server/src/controllers/preferenceController.ts
+++ server/src/controllers/preferenceController.ts
@@ -23,7 +23,24 @@ export const addPreferences: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const getPreferences: RequestHandler = async (req, res, next) => {};
+export const getPreferences: RequestHandler = async (req, res, next) => {
+  const userId = req.userId;
+
+  if (!userId) {
+    return res.status(400).json({ error: "Not authenticated." });
+  }
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM JOB_PREFERENCES WHERE user_id = $1",
+      [userId],
+    );
+
+    res.status(201).json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updatePreferences: RequestHandler = async (req, res, next) => {};
 
--- server/src/routes/preferenceRoutes.ts
+++ server/src/routes/preferenceRoutes.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import requireAuth from "../middleware/requireAuth.js";
-import { addPreferences } from "../controllers/preferenceController.js";
+import {
+  addPreferences,
+  getPreferences,
+} from "../controllers/preferenceController.js";
 
 const router = express.Router();
 
 router.use(requireAuth);
 
+router.get("/all-preferences", getPreferences);
 router.post("/preference", addPreferences);
 
 export default router;
